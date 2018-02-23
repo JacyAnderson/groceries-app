2b4b42d5780d6b3f9a8de907e03a6f31b6dd707d
Jacy <jacyjamesanderson@gmail.com>
2018-02-22 19:14:08 -0700

    Display recipes created with Recipe constructor to page

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,12 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes = [];
+  recipes: Recipe[] = [
+    new Recipe('Green Chile', 'Delicous, New Mexican Green Chile', 'https://eatyourworld.com/images/staff_food_entries/green-chili.jpg'),
+    new Recipe('Breakfast Casserole', 'A delicious breakfast casserole', 'https://food.fnr.sndimg.com/content/dam/images/food/fullset/2013/5/8/2/PB1305H_breakfast-casserole-recipe_s4x3.jpg.rend.hgtvcom.616.462.suffix/1371616662935.jpeg')
+  ];
 
   constructor() { }
 
